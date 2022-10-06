9905ebe5bb9f0e0438626598e1d460b584a1cb92
noriyoshi koutaka <nkoutaka@tesutonoMacBook-Pro.local>
2022-10-06 15:07:02 +0900

    create作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('tasks/create', [\App\Http\Controllers\TaskController::class, 'create'])->name('tasks.create');
+Route::post('tasks', [\App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
